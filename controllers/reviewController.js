cea5ee4bbcb16e972bcf61cb4926d828d69e81ef
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-04-06 14:17:31 -0400

    review routes working

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,7 +5,7 @@ const getReviewsByLocation = async (req, res) => {
         const { id } = req.params;
         console.log("id: ", id)
         const location = await Location.findById(id)
-        
+        const reviews = await Review.find({location:id})
         if (reviews) {
             return res.status(200).json({ reviews });
         }
@@ -21,7 +21,6 @@ const createReview = async (req, res) => {
         console.log("create review: ", review)
         await review.save()
         const location = await Location.findById(req.params.id)
-        location.reviews.push(review._id)
         await location.save()
         return res.status(201).json({
             review
@@ -30,32 +29,6 @@ const createReview = async (req, res) => {
         return res.status(500).json({ error: error.message })
     }
 }
- 
-// const updatePlant = async (req, res) => {
-//    try {
-//        const { id } = req.params;
-//        const plant = await Plant.findByIdAndUpdate(id, req.body, { new: true })
-//        await plant.save()
-//        return res.status(201).json({
-//            plant,
-//        });
-//    } catch (error) {
-//        res.status(500).send(error.message);
-//    }
-// }
- 
-// const deletePlant = async (req, res) => {
-//    try {
-//        const { id } = req.params;
-//        const deleted = await Plant.findByIdAndDelete(id)
-//        if (deleted) {
-//            return res.status(200).send("Plant deleted");
-//        }
-//        throw new Error("Plant not found");
-//    } catch (error) {
-//        return res.status(500).send(error.message);
-//    }
-// }
  
 module.exports = {
     getReviewsByLocation,
