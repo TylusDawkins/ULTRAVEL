8825464d69c65f80ed30accf76b91e8808cc8a39
Ran Kim <rannio@gmail.com>
2022-04-06 18:23:11 -0400

    console logged locations

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
 
-const Client = Axios.create({ baseURL: `https://localhost:3001` })
+const Client = Axios.create({ baseURL: `http://localhost:3001/locations` })
 
 export default Client
