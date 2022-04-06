d2aa5f3a8ecf49c80a80851b7eb7746d5da72942
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-04-06 08:33:20 -0700

    fixed names/imports in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import './styles/App.css'
 import { Routes, Route } from 'react-router-dom'
-import LocationList from './components/LocationList'
-import LocationDetails from './components/LocationDetails'
+import Locations from './components/Locations'
+import Reviews from './components/Reviews'
 
 
 const App = () => {
-  
+  console.log('Hello World!')
   return (
     <div className="App">
       <h3>ULTRAVEL Travel Blog</h3>
       <div className='Location-list'>
       </div>
       <Routes>
-        <Route path='/' element={<LocationList />} />
-        <Route path="/locations-list/:id" element={<LocationDetails />} />
+        <Route path='/' element={<Locations />} />
+        <Route path="/locations-list/:id" element={<Reviews />} />
       </Routes>
     </div>
   )
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { LoadReviews } from '../store/actions/MovieActions'
+import { LoadReviews } from '../store/actions/LocationActions'
 
 const mapStateToProps = ({ reviewsState }) => {
   return { reviewsState }
